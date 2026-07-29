6e7d34af48b660dbd9ee0e4c9f30d72705c8e42f
nichiyobuilder <306443015+nichiyobuilder@users.noreply.github.com>
2026-07-29 22:49:24 +0900

    fix typo

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
 		<footer class="flex justify-end gap-4 px-12 py-8 text-sm">
 			<a href="/about">About</a>
 			<a href="/terms">Terms of Service</a>
-			<a href="/privacy">Privacy Poricy</a>
+			<a href="/privacy">Privacy Policy</a>
 			<a href="/contact">Contact</a>
 		</footer>
 	);
